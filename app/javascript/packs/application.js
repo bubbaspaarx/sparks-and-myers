fcde824cddd66de205d50d5cd9104b2546d90832
Edward Sparks <edd_sparks@hotmail.com>
2018-04-18 10:21:50 +0100

    created a new project for sparks & myers

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,5 @@
+import "bootstrap";
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+import '../components/scroll';
+import '../components/testimonial';
+initUpdateNavbarOnScroll();
